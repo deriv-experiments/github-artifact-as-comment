b92085c59288617445962f8b0e14e32fcd1d6091
Mark Wylde <mark.wylde@regentmarkets.com>
2024-05-14 16:04:21 +0100

    Implement table

--- .github/workflows/compareReports.js
+++ .github/workflows/compareReports.js
@@ -1,3 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+
+function readJsonFile(filePath) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  }
+  return null;
+}
+
+function calculateDiff(oldSize, newSize) {
+  return newSize - oldSize;
+}
+
+function formatSize(size, diff) {
+  if (size === null) {
+    return '-';
+  }
+
+  const formattedSize = (size / 1024).toFixed(3) + 'kb';
+
+  if (diff === null) {
+    return formattedSize;
+  }
+
+  if (!diff) {
+    return formattedSize;
+  }
+
+  return formattedSize + ' ' + (diff < 0 ? '🟢' : '🟡');
+}
+
+const packagesDir = './packages';
+const oldPackagesDir = './old/packages';
+const packages = [...new Set([...fs.readdirSync(packagesDir), ...fs.readdirSync(oldPackagesDir)])];
+
+let tableRows = '';
+
+for (const pkg of packages) {
+  const oldReport = readJsonFile(path.join(oldPackagesDir, pkg, 'report.json'));
+  const newReport = readJsonFile(path.join(packagesDir, pkg, 'report.json'));
+
+  const oldSize = oldReport ? oldReport[0].gzipSize : null;
+  const newSize = newReport ? newReport[0].gzipSize : null;
+  const diff = oldSize && newSize ? calculateDiff(oldSize, newSize) : null;
+
+  tableRows += `
+    <tr>
+      <td>${pkg}</td>
+      <td>${formatSize(oldSize)}</td>
+      <td>${formatSize(newSize, diff)}</td>
+      <td>${diff ? formatSize(diff) : '-'}</td>
+    </tr>
+  `;
+}
+
 console.log(`
 <table>
   <thead>
@@ -6,14 +63,8 @@ console.log(`
     <th>this</th>
     <th>diff</th>
   </thead>
-
   <tbody>
-    <tr>
-      <td>test</td>
-      <td>2</td>
-      <td>3</td>
-      <td>+1</td>
-    </tr>
+    ${tableRows}
   </tbody>
 </table>
 `);
--- packages/one/src/index.js
+++ packages/one/src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello webpack', 'This is app one'], '. ');
+  element.innerHTML = _.join(['Hello webpack', 'This is the first app'], '. ');
 
   return element;
 }
